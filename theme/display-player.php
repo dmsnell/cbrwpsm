b6e81c8e133e22fd865c4ea73716f0255689d759
Dennis Snell <dennis.snell@automattic.com>
2016-06-16 16:16:56 -0700

    Update display-player.php

--- theme/display-player.php
+++ theme/display-player.php
@@ -29,3 +29,33 @@ array_walk( $playerFields, function( $field ) use ( $payer ) {
   ?><h3><?= $label ?>: <?= $transform( $player->$fieldName ) ?></h3>
 } );
 ?>
+
+<?php
+function display_fields( $player ) {  
+  $playerFields = [
+    [ 'firstName', 'First Name' ],
+    [ 'lastName', 'Last Name' ],
+    [ 'birthDate', 'Birth Date', $formatDate ]
+  ];
+
+  $display_fields = array_filter( $playerFields, function( $field ) use ( $player ) {
+    return isset( $player->$field[0] );
+  } );
+
+  return array_map( function( $field ) use ( $identity, $player ) {
+    $identity = function( $a ) { return $a; }
+    $formatDate = function( $d ) { return strftime( '%b', $d ); }
+    
+    list( $name, $label, $transform ) = array_pad( $field, 3, $identity );
+
+    return [ 'label' => $label, 'value' => $transform( $player->$name ) ];
+  }, $playerFields );
+}
+?>
+
+<?php 
+  $display_fields = RHC\Player\display_fields( $player );
+  foreach( $display_fields as $label => $value ) {
+    ?><h3><?= $label ?>: <?= $value ?></h3><?php
+  }
+?>
